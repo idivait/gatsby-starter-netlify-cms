03694dece8609c358b7f1e5f8e2b79be4981ad99
Sandra S. Templeton <10134142+idivait@users.noreply.github.com>
2018-03-05 14:02:36 -0600

    test site title query on server

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,19 +2,37 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
+import Link from 'gatsby-link'
+
 import Navbar from '../components/Navbar'
 import './all.sass'
 
-const TemplateWrapper = ({ children }) => (
+const TemplateWrapper = (data) => {
+  console.log(data);
+
+  return (
   <div>
     <Helmet title="Home | Gatsby + Netlify CMS" />
     <Navbar />
-    <div>{children()}</div>
+    <div>{data.children()}</div>
   </div>
 )
+}
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
 }
+
+export const pageQuery = graphql`
+  query RootQuery {
+    markdownRemark(id: { eq: "/about" }) {
+      html
+      frontmatter {
+        title
+        sitetitle
+      }
+    }
+  }
+`
 
 export default TemplateWrapper
